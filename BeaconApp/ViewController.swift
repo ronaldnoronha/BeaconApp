3faeb1973f103987dd9d2ad1fc39e52cef132e02
Ronald <rnor3536@uni.sydney.edu.au>
2021-10-14 22:16:56 +1100

    Initial Commit

---
+++ BeaconApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BeaconApp
+//
+//  Created by Ronald Noronha on 14/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
